e959bbde7a2d6a86b2b71b025ce5f802edf53385
Jake Sykes <jakesykes@proton.me>
2026-02-14 14:52:44 +0000

    added stuff to devShell

--- flake.nix
+++ flake.nix
@@ -9,12 +9,15 @@
     in
       pkgs.mkShell {
         buildInputs = with pkgs; [
+          gcc
           jdk21
+          maven
           spring-boot-cli
         ];
         shellHook = ''
           echo ""
           echo "Java version: $(java -version 2>&1 | head -n 1)"
+          echo "Maven version: $(mvn -version | head -n 1)"
           echo "Spring Boot CLI: $(spring --version)"
           echo ""
         '';
